fdd72f456068531988a5797e4422c7524d9a999d
8===э <andrey1mosckalenko@gmail.com>
2024-05-27 10:56:14 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import * as xePress from './server.js'
+
+xePress.listen(3000, () => {
+    console.log('Server startded on http://localhost:3000')
+})
---
+++ server.js
@@ -0,0 +1,15 @@
+import http from 'http';
+
+const handlers = {
+    'GET': {},
+    'POST': {},
+    'DELETE': {},
+}
+
+const server = http.createServer((req, res) => {
+
+})
+
+export const listen = (port, cb) => {
+    server.listen(port, cb);
+}
